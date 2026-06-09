09e1bf7298daba589c296d8a7c5fb0da134e66ff
Rubanskaya Yuliya <rubanskaaula@gmail.com>
2026-06-09 16:27:10 +0300

    Books page with Taiga UI table

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TuiTable } from '@taiga-ui/addon-table/components/table';
 import { HarryPotterService } from '../../services/harry-potter.service';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiTable],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss'
 })
 export class BooksComponent implements OnInit {
   books: any[] = [];
+  columns = ['title', 'releaseDate', 'pages', 'description'];
 
   constructor(private harryPotterService: HarryPotterService) {}
 
