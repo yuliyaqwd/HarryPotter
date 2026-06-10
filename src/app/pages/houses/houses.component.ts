5c4602c57fb6168ae44e22e205a630fd177e03a0
Rubanskaya Yuliya <rubanskaaula@gmail.com>
2026-06-10 14:59:52 +0300

    Add houses page with Taiga UI cards

--- src/app/pages/houses/houses.component.ts
+++ src/app/pages/houses/houses.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TuiCardLarge } from '@taiga-ui/layout/components/card';
+import { HarryPotterService } from '../../services/harry-potter.service';
 
 @Component({
   selector: 'app-houses',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TuiCardLarge],
   templateUrl: './houses.component.html',
   styleUrl: './houses.component.scss'
 })
-export class HousesComponent {
+export class HousesComponent implements OnInit {
+  houses: any[] = [];
 
+  constructor(private harryPotterService: HarryPotterService) {}
+
+  ngOnInit() {
+    this.harryPotterService.getHouses().subscribe(data => {
+      this.houses = data;
+    });
+  }
 }
