5c433344f0220b47cb654de75f08838c0f4723cb
Rubanskaya Yuliya <rubanskaaula@gmail.com>
2026-06-10 15:58:18 +0300

    Add characters table

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TuiTable } from '@taiga-ui/addon-table/components/table';
+import { HarryPotterService } from '../../services/harry-potter.service';
 
 @Component({
   selector: 'app-characters',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TuiTable],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.scss'
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit {
+  characters: any[] = [];
+  columns = ['fullName', 'birthdate', 'hogwartsHouse', 'nickname'];
 
+  constructor(private harryPotterService: HarryPotterService) {}
+
+  ngOnInit() {
+    this.harryPotterService.getCharacters().subscribe(data => {
+      this.characters = data;
+    });
+  }
 }
