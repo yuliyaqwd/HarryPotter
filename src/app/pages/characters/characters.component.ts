e6855fc1426fb901bd1db26bdc2a7a1ef515f2e9
Rubanskaya Yuliya <rubanskaaula@gmail.com>
2026-06-11 14:34:30 +0300

    Add house hint with colored border on characters page

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiTable } from '@taiga-ui/addon-table/components/table';
+import { TuiHint } from '@taiga-ui/core/directives/hint';
 import { HarryPotterService } from '../../services/harry-potter.service';
 
 @Component({
   selector: 'app-characters',
   standalone: true,
-  imports: [CommonModule, TuiTable],
+  imports: [CommonModule, TuiTable, TuiHint],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.scss'
 })
 export class CharactersComponent implements OnInit {
   characters: any[] = [];
+  houses: any[] = [];
   columns = ['fullName', 'birthdate', 'hogwartsHouse', 'nickname'];
 
   constructor(private harryPotterService: HarryPotterService) {}
@@ -20,5 +22,17 @@ export class CharactersComponent implements OnInit {
     this.harryPotterService.getCharacters().subscribe(data => {
       this.characters = data;
     });
+    this.harryPotterService.getHouses().subscribe(data => {
+      this.houses = data;
+    });
+  }
+
+  getHouse(houseName: string) {
+    return this.houses.find(h => h.house === houseName);
+  }
+
+  getNameColor(colors: string[]): string {
+    const lightColors = ['yellow', 'gold', 'white', 'silver'];
+    return lightColors.includes(colors[0]) ? colors[1] : colors[0];
   }
 }
